2cceb3a34e95ee17d08adbedf0960f4a9c7a6e75
mrnovado <novado@ymail.com>
2020-10-02 15:48:55 +0300

    basic graph-ql integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,112 +1,87 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar} from 'react-native';
+import {ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery} from '@apollo/client';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import env from './env.json';
 
-declare const global: {HermesInternal: null | {}};
+const {REACT_APP_API_ENDPOINT} = env as Record<string, string>;
 
-const App = () => {
+const client = new ApolloClient({
+  uri: REACT_APP_API_ENDPOINT,
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Content />
+    </ApolloProvider>
+  );
+}
+
+function Content() {
+  const {data, loading, error, refetch} = useQuery(gql`
+    query MyQuery {
+      usersList {
+        count
+        items {
+          id
+          email
+          firstName
+        }
+      }
+    }
+  `);
+
+  const queryResult = (function renderQ() {
+    if (loading) {
+      return <Text>Loading!</Text>;
+    }
+
+    if (error) {
+      const message = JSON.stringify(error, null, 2);
+      return <Text style={styles.error}>{message}</Text>;
+    }
+
+    if (data) {
+      const message = JSON.stringify(data, null, 2);
+      return <Text>{message}</Text>;
+    }
+
+    return <Text>Unknown Query State!</Text>;
+  })();
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+        <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
+          <View>
+            <Text>URI: {REACT_APP_API_ENDPOINT}</Text>
           </View>
+          <View style={styles.body}>{queryResult}</View>
         </ScrollView>
       </SafeAreaView>
     </>
   );
-};
+}
 
 const styles = StyleSheet.create({
+  error: {
+    color: 'red',
+  },
   scrollView: {
-    backgroundColor: Colors.lighter,
+    backgroundColor: 'white',
   },
   engine: {
     position: 'absolute',
     right: 0,
   },
   body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
+    backgroundColor: 'white',
   },
   footer: {
-    color: Colors.dark,
+    color: 'black',
     fontSize: 12,
     fontWeight: '600',
     padding: 4,
