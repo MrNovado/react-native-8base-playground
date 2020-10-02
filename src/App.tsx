dab6573e9a8d4b620aec974b5552990e1de2d8f8
mrnovado <novado@ymail.com>
2020-10-02 17:01:50 +0300

    paper & styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar} from 'react-native';
-import {ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery} from '@apollo/client';
+import React from "react";
+import { Text, StatusBar } from "react-native";
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery } from "@apollo/client";
 
-import env from './env.json';
+import styled from "styled-components/native";
+import { Provider as PaperProvider, ActivityIndicator, Appbar, Colors, Button } from "react-native-paper";
 
-const {REACT_APP_API_ENDPOINT} = env as Record<string, string>;
+import env from "./env.json";
+
+const { REACT_APP_API_ENDPOINT } = env as Record<string, string>;
 
 const client = new ApolloClient({
   uri: REACT_APP_API_ENDPOINT,
@@ -14,38 +17,65 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Content />
+      <PaperProvider>
+        <Content />
+      </PaperProvider>
     </ApolloProvider>
   );
 }
 
+const AppContentContainer = styled.SafeAreaView`
+  flex: 1;
+`;
+
+const ContentView = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: ${Colors.lightBlue100};
+`;
+
+const ErrorMessage = styled.Text`
+  color: ${Colors.red500};
+`;
+
 function Content() {
-  const {data, loading, error, refetch} = useQuery(gql`
-    query MyQuery {
-      usersList {
-        count
-        items {
-          id
-          email
-          firstName
+  const { data, loading, error, refetch } = useQuery(
+    gql`
+      query MyQuery {
+        usersList {
+          count
+          items {
+            id
+            email
+            firstName
+          }
         }
       }
-    }
-  `);
+    `,
+    { fetchPolicy: "no-cache" },
+  );
 
   const queryResult = (function renderQ() {
     if (loading) {
-      return <Text>Loading!</Text>;
+      return <ActivityIndicator animating size="large" color={Colors.red500} />;
     }
 
     if (error) {
       const message = JSON.stringify(error, null, 2);
-      return <Text style={styles.error}>{message}</Text>;
+      return <ErrorMessage>{message}</ErrorMessage>;
     }
 
     if (data) {
       const message = JSON.stringify(data, null, 2);
-      return <Text>{message}</Text>;
+      return (
+        <>
+          <Text>{message}</Text>
+          <Button icon="restart" mode="contained" onPress={refetch}>
+            Refetch
+          </Button>
+        </>
+      );
     }
 
     return <Text>Unknown Query State!</Text>;
@@ -54,40 +84,15 @@ function Content() {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
-          <View>
-            <Text>URI: {REACT_APP_API_ENDPOINT}</Text>
-          </View>
-          <View style={styles.body}>{queryResult}</View>
-        </ScrollView>
-      </SafeAreaView>
+      <AppContentContainer>
+        <ContentView>{queryResult}</ContentView>
+        <Appbar>
+          <Appbar.Action icon="restart" onPress={refetch} />
+          <Appbar.Content title="URI" subtitle={REACT_APP_API_ENDPOINT} />
+        </Appbar>
+      </AppContentContainer>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  error: {
-    color: 'red',
-  },
-  scrollView: {
-    backgroundColor: 'white',
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: 'white',
-  },
-  footer: {
-    color: 'black',
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
